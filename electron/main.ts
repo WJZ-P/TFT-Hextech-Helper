d743d1a196e640f4161d92ce0955f73fbd40fabf
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2025-09-25 01:04:34 +0800

    写点东西

--- electron/main.ts
+++ electron/main.ts
@@ -3,6 +3,14 @@ import { createRequire } from 'node:module'
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 
+/**
+ * 下面这两行代码是历史原因，新版的ESM模式下需要CJS里面的require、__dirname来提供方便
+ * import.meta.url：file:///C:/Users/YourProject/electron/main.ts (一个标准的 URL 格式路径)。
+ * path.dirname：把一个完整的文件路径，只剪下它所在的文件夹部分。
+ * 这里的__dirname就是我们手造出来的。
+ *
+ * 然后require也同理，是我们手搓的，因为新版ESM不提供require。
+ */
 const require = createRequire(import.meta.url)
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
@@ -28,9 +36,9 @@ let win: BrowserWindow | null
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),//  窗口左上角的图标
     webPreferences: {
-      preload: path.join(__dirname, 'preload.mjs'),
+      preload: path.join(__dirname, 'preload.mjs'),// 指定preload文件
     },
   })
 
@@ -39,6 +47,7 @@ function createWindow() {
     win?.webContents.send('main-process-message', (new Date).toLocaleString())
   })
 
+  //  判断是在开发环境还是打包好的程序
   if (VITE_DEV_SERVER_URL) {
     win.loadURL(VITE_DEV_SERVER_URL)
   } else {
