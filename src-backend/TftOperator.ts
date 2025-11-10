1fe3cab4f8e020c3c7234bcfa4e58892ee2be814
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2025-11-11 00:58:46 +0800

    feat(TftOperator): 添加游戏窗口位置计算功能
    添加通过electron获取屏幕尺寸并计算游戏窗口位置的功能，包括初始化方法和相关日志记录

--- src-backend/TftOperator.ts
+++ src-backend/TftOperator.ts
@@ -1,12 +1,24 @@
 //  游戏分辨率是1024x768
-import { mouse, Button, Point, findWindow, Region } from "@nut-tree-fork/nut-js";
-export class TftOperator {
-    private static instance:TftOperator;
-    //  缓存游戏窗口的位置和大小
-    private gameWindowRegion:Region;
+import {mouse, Button, Point, Region} from "@nut-tree-fork/nut-js";
+import {logger} from "./utils/Logger";
+import { screen } from "electron";
+
+const GAME_WIDTH = 1024;
+const GAME_HEIGHT = 768;
+
+export enum ShopSLot{
+    SHOP_SLOT_1
+}
+
+
+class TftOperator {
+    private static instance: TftOperator;
+    //  缓存游戏窗口的左上角坐标
+    private gameWindowRegion: Point;
 
     private constructor() {
     }
+
     public static getInstance(): TftOperator {
         if (!TftOperator.instance) {
             TftOperator.instance = new TftOperator();
@@ -14,9 +26,37 @@ export class TftOperator {
         return TftOperator.instance;
     }
 
-    private async clickAt(offset:Point){
+    /**
+     * 初始化，通过electron找到屏幕中心点，LOL窗口默认居中，以此判断布局。
+     */
+    public init(): boolean {
+        try {
+            // 从electron获取屏幕尺寸
+            const primaryDisplay = screen.getPrimaryDisplay();
+            const { width: screenWidth, height: screenHeight } = primaryDisplay.size;
+            // b. (关键) 计算屏幕中心
+            const screenCenterX = screenWidth / 2;
+            const screenCenterY = screenHeight / 2;
+
+            // c. (关键) 计算游戏窗口的左上角 (0,0) 点
+            const originX = screenCenterX - (GAME_WIDTH / 2);
+            const originY = screenCenterY - (GAME_HEIGHT / 2);
+
+            this.gameWindowRegion = new Point(originX, originY);
+            logger.info(`[TftOperator] 屏幕尺寸: ${screenWidth}x${screenHeight}.`);
+            logger.info(`[TftOperator] 游戏基准点 (0,0) 已计算在: (${originX}, ${originY})`);
+            return true;
+
+        } catch (e: any) {
+            logger.error(`[TftOperator] 无法从 Electron 获取屏幕尺寸: ${e.message}`);
+            this.gameWindowRegion = null;
+            return false;
+        }
+    }
+
+    private async clickAt(offset: Point) {
 
     }
 }
 
-//node-window-manager
+export const tftOperator = TftOperator.getInstance();
